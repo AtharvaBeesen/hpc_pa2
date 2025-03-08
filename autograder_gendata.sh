dbee21d65171839cc71b6a7cde75c6e68efa9d97
Atharva Beesen <86427671+AtharvaBeesen@users.noreply.github.com>
2025-03-07 22:43:14 -0500

    Update autograder_gendata.sh

--- autograder_gendata.sh
+++ autograder_gendata.sh
@@ -5,32 +5,32 @@ echo " "
 mkdir -p autograderdata
 
 if [ ! -f "inputard_4.txt" ]; then
-    python3 ard_gen.py 15
+    python3 ard_gen.py 4
 fi
 echo " "
 
 if [ ! -f "inputm2_4.txt" ]; then
-    python3 m2m_gen.py 15
+    python3 m2m_gen.py 4
 fi
 echo " "
 
 if [ ! -f "inputard_8.txt" ]; then
-    python3 ard_gen.py 24
+    python3 ard_gen.py 8
 fi
 echo " "
 
 if [ ! -f "inputm2_8.txt" ]; then
-    python3 m2m_gen.py 24
+    python3 m2m_gen.py 8
 fi
 
 
 if [ ! -f "inputard_16.txt" ]; then
-    python3 ard_gen.py 15
+    python3 ard_gen.py 16
 fi
 echo " "
 
 if [ ! -f "inputm2_16.txt" ]; then
-    python3 m2m_gen.py 15
+    python3 m2m_gen.py 16
 fi
 echo " "
 
